ae65bc490b4829bd2deb41f5c3b19be0c6e6d3dc
Ankit Sablok <ankitsablok19091989@gmail.com>
2014-07-02 17:54:45 -0400

    Project - 1 - Twitter Data Sentiment Analysis - Commit - 4

--- Project - 1 - Twitter Sentiment Analysis/assignment1/tweet_sentiment.py
+++ Project - 1 - Twitter Sentiment Analysis/assignment1/tweet_sentiment.py
@@ -3,28 +3,20 @@
 # Institution - University at Buffalo, The State University of New York
 # Course - Introduction To Data Science
 
-# import Python's system module
+# import Python's system module and json module
 import sys
+import json
 
-# this function reads a file line by line and returns a list of 2-tuples of the form [(word,sentiment_score)]
-def returnListOfTwoTuples(file_handle):
+# this function read the tweet file and returns a list of list of constitutent words in a tweet
+def returnListOfListOfWordsInATweet(tweet_file_handle):
 
-    # this is the list that will consist all the 2-tuples of words and their sentiment scores
-    listOfTuples = []
+    # start iterating through the file consisting of tweets line by line
+    for tweet in tweet_file_handle:
+        if '"created_at":' in tweet:
+            # use the json.loads(jsonString) function to get a dictionary data structure in which we have a text field consisting of the text of the tweet
+            tweetDictionary = json.loads(tweet)
+            print tweetDictionary
 
-    # start iterating on the file consisting of lines
-    for line in file_handle:
-        # strip off any leading or trailing whitespaces using the strip() function on string or the line just read
-        line = line.strip()
-
-        # split the line on the basis of the "\t" TAB character
-        splitLine = line.split("\t")
-
-        # convert the list of strings obtained above into a tuple using the tuple() conversion function and add it to the list
-        listOfTuples.append(tuple(splitLine))
-
-    # return the list of tuples formed
-    return listOfTuples
 
 # this is the main function which acts a  driver for evaluating the sentiment scores for the tweets
 def main():
@@ -34,7 +26,7 @@ def main():
     # this variable stores the file handle for the tweet file
     tweet_file_handle = open(sys.argv[2],'r')
 
-    print returnListOfTwoTuples(sentiment_file_handle)
+    returnListOfListOfWordsInATweet(tweet_file_handle)
 
 if __name__ == "__main__":
     main()
